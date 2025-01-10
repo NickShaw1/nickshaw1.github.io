2f579cdf12fd42187003fd3f94f4c77f96ef2db9
NickShaw1 <nickshawcontact@gmail.com>
2025-01-10 13:47:19 +0000

    new accordians

---
+++ exercises/accordian/accordian.js
@@ -0,0 +1,8 @@
+const btns = document.querySelectorAll(".question-btn");
+
+btns.forEach(function (btn) {
+  btn.addEventListener("click", function (e) {
+    const question = e.currentTarget.closest(".question");
+    question.classList.toggle("show-text");
+  });
+});
